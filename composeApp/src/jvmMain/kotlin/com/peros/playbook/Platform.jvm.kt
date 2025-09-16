c1810a4a9a712e975f994fad0736bb9fed735517
paleros <erospali01@gmail.com>
2025-09-16 17:42:58 +0200

    Initial commit

---
+++ composeApp/src/jvmMain/kotlin/com/peros/playbook/Platform.jvm.kt
@@ -0,0 +1,7 @@
+package com.peros.playbook
+
+class JVMPlatform: Platform {
+    override val name: String = "Java ${System.getProperty("java.version")}"
+}
+
+actual fun getPlatform(): Platform = JVMPlatform()
